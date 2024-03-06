65b7cf83eceb835c223d50a6e31a600343b8f67f
juyeon319 <127908302+juyeon319@users.noreply.github.com>
2024-03-06 14:47:18 +0900

    [Bronze IV] Title: 주사위 세개, Time: 200 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int num1 = scan.nextInt(); 
+		int num2 = scan.nextInt(); 
+		int num3 = scan.nextInt();
+		
+		int maxNum = Math.max(Math.max(num1, num2), num3);
+		
+		if(num1 == num2 && num1 == num3)
+			System.out.println(10000 + num1 * 1000);
+		else if(num1 == num2 || num1 == num3)
+			System.out.println(1000 + num1 * 100);
+		else if(num2 == num3)
+			System.out.println(1000 + num2 * 100);
+		else
+			System.out.println(maxNum * 100);
+	}
+}
