4ddd17127788c776cdcdec90f39058cdac76db4a
juyeon319 <127908302+juyeon319@users.noreply.github.com>
2024-03-06 14:43:59 +0900

    [Bronze V] Title: 사칙연산, Time: 212 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+/*
+6. 두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오.  
+*/
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int A = scan.nextInt();
+		int B = scan.nextInt();
+		
+		int result1 = A+B;
+		int result2 = A-B;
+		int result3 = A*B;
+		int result4 = A/B;
+		int result5 = A%B;
+		
+		System.out.println(result1);
+		System.out.println(result2);
+		System.out.println(result3);
+		System.out.println(result4);
+		System.out.println(result5);
+	}
+}
