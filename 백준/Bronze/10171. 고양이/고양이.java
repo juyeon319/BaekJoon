a91016bd981f02ec1908eb6a9821f80ba079839b
juyeon319 <127908302+juyeon319@users.noreply.github.com>
2024-03-06 14:46:02 +0900

    [Bronze V] Title: 고양이, Time: 120 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,13 @@
+
+
+/*
+12. 아래 예제와 같이 고양이를 출력하시오. 
+*/
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
