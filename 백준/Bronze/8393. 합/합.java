2bd63f2e66ead9bbc04b7d902ad40bff1a266fc0
juyeon319 <127908302+juyeon319@users.noreply.github.com>
2024-03-24 22:49:39 +0900

    [Bronze V] Title: 합, Time: 204 ms, Memory: 17680 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt();
+		int N = scan.nextInt();
+		
 		int sum = 0;
 		
-		for(int i=1; i<=n; i++) {
+		for(int i=1; i<=N; i++) {
 			sum += i;
 		}
 		System.out.println(sum);
